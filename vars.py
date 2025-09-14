478f79a7d3785ebdff323774dbdf6d5643e9d71e
hellobhavesh795 <mrbhavwsh09@gmail.com>
2025-09-14 15:25:42 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,10 +2,10 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-ADMIN     = os.environ.get("ADMIN", "1977145189")  
+API_ID    = os.environ.get("API_ID", "21866171")
+API_HASH  = os.environ.get("API_HASH", "5788dba8f23fade5edda55948e985f06")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8147399877:AAEk1DTB1Hvy52wsjM96d2xzID83_0C5s1g") 
+ADMIN     = os.environ.get("ADMIN", "8082516546")  
 ADMINS = [1977145189]  # Replace with your real Telegram ID
 # Replace with your Telegram user ID
 
